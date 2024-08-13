77c0f21f8b750472e5d0bb21746b9ddfd495b8a1
NezukoTierna <matiasorellanahormazabal@gmail.com>
2024-08-12 21:51:27 -0400

    feat: added ngif

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -10,6 +10,29 @@ export class HeroComponent {
   public name: string = 'IronMan';
   public age: number = 45;
 
+  get capitalizedName(): string {
+    return 'Hola Mundo'.toUpperCase();
+  }
 
+  getHeroDescription(): string {
+    return `${ this.name } ${ this.age }`;
+  }
+
+  changeHero(): void {
+
+    this.name = 'Spiderman'
+
+  }
+
+  changeAge() {
+
+    this.age = 30
+
+  }
+
+  resetForm(): void {
+    this.name = 'IronMan';
+    this.age = 45;
+  }
 
 }
