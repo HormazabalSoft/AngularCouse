6de587a1554399a09d52354871bab8e11ae527e5
NezukoTierna <matiasorellanahormazabal@gmail.com>
2024-08-12 22:39:42 -0400

    refactor: counters are moved in the countermodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
+import { CounterComponent } from './components/counter/partComponents/counter/counter.component';
 import { HeroComponent } from './heroes/hero/hero.component';
 import { ListComponent } from './heroes/list/list.component';
+import { CounterModule } from './components/counter/counter.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent,
+    //CounterComponent,
     HeroComponent,
     ListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CounterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
