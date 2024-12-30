d19b907974f3affc3d0f836e95e83c83a34ec354
DarkHormazabal <morgulgamer@gmail.com>
2024-12-29 21:24:45 -0300

    feat: video 99

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { GifsService } from '../../../gifs/services/gifs.service';
+import { Onclick } from '../../../gifs/interfaces/gifs.interfaces';
 
 @Component({
   selector: 'shared-sidebar',
@@ -12,6 +13,12 @@ export class SidebarComponent {
 
   get tagsHistory() {
     return this.gifsService.tagsHistory;
+  }
+
+  Onclick(gifTag: string){
+
+    this.gifsService.searchTag(gifTag);
+
   }
 
 }
