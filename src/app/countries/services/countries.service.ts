e886487151f870d7268b8b1e2d68228753d5b9c7
MatiassOrellana <matias.orellana@alumnos.ucn.cl>
2025-01-03 12:02:48 -0300

    feat: video 126 completed table of contries

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, map, Observable, of, tap } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({
@@ -15,7 +15,13 @@ export class CountriesService {
   searchCapital( term: string ): Observable<any>{
 
     const url = `${ this.apiURL }/capital/${ term }`;
-    return this.http.get<any[]>(url);
+    return this.http.get<any[]>(url).pipe(
+      catchError( error => {
+        console.log(error);
+
+        return of([])
+      })
+    );
 
   }
 }
